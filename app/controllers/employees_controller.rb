698d2649d790f15e314ea7bee92fcc40fffe93f7
github username <github email address>
2019-09-25 15:45:21 -0400

    did with 082619 cohort as practice

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,30 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @e = Employee.find_by(id: params[:id])
+    end
+
+    def new
+        @e = Employee.new
+    end
+
+    def create
+        @e = Employee.new(e_params)
+        if @e.save
+            redirect_to employee_path(@e)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def e_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
+
 end
